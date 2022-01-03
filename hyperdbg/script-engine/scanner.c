b7d256aab1095171ddf2d854553cfd74023c488e
mhgholamrezaei <mgh27.1997@gmail.com>
2022-01-03 23:37:05 +0900

    fixed boolean expression bug

--- hyperdbg/script-engine/scanner.c
+++ hyperdbg/script-engine/scanner.c
@@ -532,7 +532,6 @@ GetToken(char * c, char * str)
             uint8_t NotHex = 0;
             do
             {
-                char debug_tmp = *c;
                 if (*c != '`')
                     Append(Token, *c);
 
