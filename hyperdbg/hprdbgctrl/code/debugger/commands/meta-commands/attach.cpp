05a68e4286a19d54c72bb06cc7dcf0c390acf4dc
SinaKarvandi <ms.karvandi@yahoo.com>
2022-03-03 00:27:34 +0330

    disable user-debugger

--- hyperdbg/hprdbgctrl/code/debugger/commands/meta-commands/attach.cpp
+++ hyperdbg/hprdbgctrl/code/debugger/commands/meta-commands/attach.cpp
@@ -15,6 +15,7 @@
 // Global Variables
 //
 extern BOOLEAN g_IsSerialConnectedToRemoteDebuggee;
+extern BOOLEAN g_IsSerialConnectedToRemoteDebugger;
 
 /**
  * @brief help of .attach command
@@ -43,13 +44,21 @@ CommandAttach(vector<string> SplittedCommand, string Command)
     BOOLEAN NextIsPid = FALSE;
 
     //
-    // Show a message that the user debugger is still in the experimental version
+    // Disable user-mode debugger in this version
     //
-    ShowMessages("in contrast with the kernel debugger, the user debugger is still very basic "
-                 "and needs a lot of tests and improvements. It's highly recommended not to run the "
-                 "user debugger in your bare metal system. Instead, run it on a supported virtual "
-                 "machine to won't end up with a Blue Screen of Death (BSOD) in your primary device. "
-                 "Please keep reporting the issues to improve the user debugger\n\n");
+#if ActivateUserModeDebugger == FALSE
+
+    if (!g_IsSerialConnectedToRemoteDebugger)
+    {
+        ShowMessages("The user-mode debugger is still in the beta version and not stable. "
+                     "We decided to exclude it from this release and release it in future versions. "
+                     "If you want to test the user-mode debugger in VMI Mode, you should build "
+                     "HyperDbg with special instructions. \nPlease follow the steps here: "
+                     "https://docs.hyperdbg.org/getting-started/build-and-install \n");
+        return;
+    }
+
+#endif // !ActivateUserModeDebugger
 
     //
     // It's a attach to a target PID
