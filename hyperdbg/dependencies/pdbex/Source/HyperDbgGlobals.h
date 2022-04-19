62373e3dc0f4dc427fbdadf46c471be278becb7c
SinaKarvandi <ms.karvandi@yahoo.com>
2022-04-20 02:08:20 +0430

    remove pdbex to add it as submodule

--- hyperdbg/dependencies/pdbex/Source/HyperDbgGlobals.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//
-// Global variables
-//
-extern Callback g_MessageHandler;
-extern BOOLEAN  g_IsOutputToFile;
-extern BOOLEAN  g_ShowInOffestFormat;
-extern CHAR *   g_MappingBufferAddress;
