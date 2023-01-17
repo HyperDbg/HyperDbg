80fd654ca44df0b6e4b7be7760cb3eb3efeeeb38
unknown <sina@hyperdbg.org>
2023-01-17 17:16:29 +0900

    move the debugger directory from hv to kd

--- hyperdbg/hprdbgkd/header/pch.h
+++ hyperdbg/hprdbgkd/header/pch.h
@@ -52,3 +52,32 @@
 #include "globals/Global.h"
 #include "driver/Driver.h"
 #include "driver/Loader.h"
+
+//
+// Debugger Types
+//
+#include "header/debugger/core/Debugger.h"
+//
+// Debugger Sub-types
+//
+#include "header/debugger/transparency/Transparency.h"
+#include "header/debugger/tests/KernelTests.h"
+#include "header/debugger/broadcast/DpcRoutines.h"
+#include "header/debugger/core/DebuggerEvents.h"
+#include "header/debugger/script-engine/ScriptEngine.h"
+#include "header/debugger/kernel-level/Kd.h"
+#include "header/debugger/user-level/Ud.h"
+#include "header/debugger/core/Dispatch.h"
+#include "header/debugger/features/Hooks.h"
+#include "header/debugger/broadcast/Broadcast.h"
+#include "header/debugger/commands/BreakpointCommands.h"
+#include "header/debugger/commands/DebuggerCommands.h"
+#include "header/debugger/commands/ExtensionCommands.h"
+#include "header/debugger/commands/Callstack.h"
+#include "header/debugger/communication/SerialConnection.h"
+#include "header/debugger/objects/Process.h"
+#include "header/debugger/objects/Thread.h"
+#include "header/debugger/user-level/Attaching.h"
+#include "header/debugger/core/Termination.h"
+#include "header/debugger/user-level/UserAccess.h"
+#include "header/debugger/user-level/ThreadHolder.h"
