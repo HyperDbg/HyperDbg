654108359f7816f45fb5e8eb6bcc5931d4e437e1
unknown <sina@hyperdbg.org>
2024-04-28 22:05:54 +0900

    add hwdbg headers

---
+++ hyperdbg/include/SDK/Headers/HardwareDebugger.h
@@ -0,0 +1,24 @@
+/**
+ * @file HardwareDebugger.h
+ * @author Sina Karvandi (sina@hyperdbg.org)
+ * @brief HyperDbg's Hardware Debugger (hwdbg) types and constants
+ * @details This file contains definitions of hwdbg elements
+ * used in HyperDbg
+ * @version 0.9
+ * @date 2024-04-28
+ *
+ * @copyright This project is released under the GNU Public License v3.
+ *
+ */
+#pragma once
+
+/**
+ * @brief Different action of hwdbg
+ * @warning This file should be changed along with hwdbg files
+ *
+ */
+typedef enum _HWDBG_ACTION
+{
+    hwdbgActionSendVersion = 0,
+    hwdbgActionSendPinInformation,
+} HWDBG_ACTION;
