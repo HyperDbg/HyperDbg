8db7cc7192ec6db76fea8d3c3d3a560fc146ed10
unknown <sina@hyperdbg.org>
2024-05-12 16:07:41 +0900

    add other environment definitions

--- hyperdbg/include/platform/kernel/header/Environment.h
+++ hyperdbg/include/platform/kernel/header/Environment.h
@@ -21,6 +21,12 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 #    define ENV_WINDOWS
+#elif defined(__linux__)
+#    error "This code cannot compile on Linux yet"
+#    define ENV_LINUX
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+#    error "This code cannot compile on BSD yet"
+#    define ENV_BSD
 #else
-#    error "This code cannot compile on non windows platforms"
+#    error "This code cannot compile on non-Windows, non-Linux, and non-BSD platforms"
 #endif
