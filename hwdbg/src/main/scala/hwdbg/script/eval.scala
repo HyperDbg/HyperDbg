4d2f458752bdee092d411b9890cb4d5d0748c936
Sina Karvandi <sina@hyperdbg.org>
2024-06-13 21:47:03 +0900

    add script eval structures

--- hwdbg/src/main/scala/hwdbg/script/eval.scala
+++ hwdbg/src/main/scala/hwdbg/script/eval.scala
@@ -58,10 +58,69 @@ class ScriptEngineEval(
   //
   val nextStage = WireInit(0.U(log2Ceil(instanceInfo.maximumNumberOfStages).W))
 
+  //
+  // Assign operator value (split the signal into only usable part)
+  //
+  LogInfo(debug)("Usable size of Value in the SYMBOL: " + ScriptOperators().getWidth)
+  val mainOperatorValue = io.stageConfig.stageSymbol.Value(ScriptOperators().getWidth - 1, 0).asTypeOf(ScriptOperators())
+
+  //-------------------------------------------------------------------------
+  // Get value module
+  //
+  val getValueModuleOutput = Wire(Vec(instanceInfo.maximumNumberOfSupportedGetScriptOperators, UInt(instanceInfo.scriptVariableLength.W)))
+
+  for (i <- 0 until instanceInfo.maximumNumberOfSupportedGetScriptOperators) {
+
+      getValueModuleOutput(i) := ScriptEngineGetValue(
+        debug,
+        instanceInfo
+    )(
+        io.en,
+        io.stageConfig.getOperatorSymbol(i),
+        io.stageConfig.pinValues
+    )
+  }
+  
+  //-------------------------------------------------------------------------
+  // Set value module
+  //
+  val setValueModuleInput = Wire(Vec(instanceInfo.maximumNumberOfSupportedSetScriptOperators, Vec(instanceInfo.numberOfPins, UInt(1.W))))
+
+  val testValueToSet = Wire(UInt(instanceInfo.scriptVariableLength.W)) // operators should be applied to this wire
+  testValueToSet := 0. U ////////////////////// Test should be removed
+
+  for (i <- 0 until instanceInfo.maximumNumberOfSupportedSetScriptOperators) {
+
+  setValueModuleInput(i) := ScriptEngineSetValue(
+        debug,
+        instanceInfo
+    )(
+        io.en,
+        io.stageConfig.setOperatorSymbol(i),
+        testValueToSet
+    )
+  }
 
   //-------------------------------------------------------------------------
   // *** Implementing the evaluation engine ***
   //
+  //
+
+  //
+  // Apply the chip enable signal
+  //
+  when(io.en === true.B) {
+
+    switch(mainOperatorValue) {
+
+      is(sFuncInc) { 
+        
+      }
+      is(sFuncDec) {
+        
+      }
+    }
+  }
 
 
   // ---------------------------------------------------------------------
@@ -75,7 +134,7 @@ class ScriptEngineEval(
   // Connect the output signals
   //
   io.nextStage := nextStage
-  io.outputPin := io.stageConfig.pinValues
+  io.outputPin := setValueModuleInput(0)
 
 }
 
