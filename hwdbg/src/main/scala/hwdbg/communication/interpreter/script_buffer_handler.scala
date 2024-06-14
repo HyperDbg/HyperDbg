0e8bccba96ca8728812d97d3f8dee02e09d643a1
Sina Karvandi <sina@hyperdbg.org>
2024-06-14 19:00:22 +0900

    add initial logic of BRAM config module

--- hwdbg/src/main/scala/hwdbg/communication/interpreter/script_buffer_handler.scala
+++ hwdbg/src/main/scala/hwdbg/communication/interpreter/script_buffer_handler.scala
@@ -23,11 +23,23 @@ import hwdbg.configs._
 import hwdbg.types._
 import hwdbg.script._
 
+object InterpreterScriptBufferHandlerEnums {
+  object State extends ChiselEnum {
+    val sIdle, sReadSizeOfBuffer, sReadTypeOfOperator, sReadValueOfOperator, sDone = Value
+  }
+}
+
 class InterpreterScriptBufferHandler(
     debug: Boolean = DebuggerConfigurations.ENABLE_DEBUG,
     instanceInfo: HwdbgInstanceInformation,
     bramDataWidth: Int
 ) extends Module {
+
+  //
+  // Import state enum
+  //
+  import InterpreterScriptBufferHandlerEnums.State
+  import InterpreterScriptBufferHandlerEnums.State._
 
   val io = IO(new Bundle {
 
@@ -49,14 +61,177 @@ class InterpreterScriptBufferHandler(
     //
     val finishedConfiguration = Output(Bool()) // whether configuration finished or not?
     val moveToNextStage = Output(Bool()) // whether configuration finished configuring the current stage or not?
+    val configureStage = Output(Bool()) // whether the configuration of stage should start or not?
     val targetOperator = Output(new HwdbgShortSymbol(instanceInfo.scriptVariableLength)) // Current operator to be configured
   })
 
-  // ---------------------------------------------------------------------
+  //
+  // State registers
+  //
+  val state = RegInit(sIdle)
+
+  //
+  // Internal registers
+  //
+  val regScriptNumberOfSymbols = Reg(UInt(bramDataWidth.W))
+
+  //
+  // Output pins
+  //
+  val readNextData = WireInit(false.B)
+
+  val finishedConfiguration = WireInit(false.B)
+  val configureStage = WireInit(false.B)
+  val moveToNextStage = WireInit(false.B)
+  val regTargetOperator = Reg(new HwdbgShortSymbol(instanceInfo.scriptVariableLength))
+
+
+  //
+  // Apply the chip enable signal
+  //
+  when(io.en === true.B) {
+
+    switch(state) {
+
+      is(sIdle) { 
+
+        //
+        // Read next data for the size of the buffer
+        //
+        readNextData := true.B
+
+        //
+        // Move to the next state
+        //
+        state := sReadSizeOfBuffer
+      }
+      is(sReadSizeOfBuffer) { 
+
+        when(io.dataValidInput) {
+
+            //
+            // Data is valid and number of symbols is now available
+            //
+            regScriptNumberOfSymbols := io.receivingData
+
+            //
+            // Request next data
+            //
+            readNextData := true.B
+
+            //
+            // Move to the configuration state
+            //
+            state := sReadTypeOfOperator
+
+        }.otherwise{
+
+            //
+            // Stay at the same state since the data is not yet received
+            //
+            state := sReadSizeOfBuffer
+        }
+      }
+      is(sReadTypeOfOperator) {
+
+
+        when(io.dataValidInput) {
+
+            //
+            // Configure the stages
+            //
+            configureStage := true.B
+
+            //
+            // Request next data
+            //
+            readNextData := true.B
+
+            //
+            // Read the operator's "Type" data
+            //
+            regTargetOperator.Type := io.receivingData
+
+            //
+            // Next, we need to read "Value"
+            //
+            state := sReadValueOfOperator
+
+        } .otherwise {
+
+            //
+            // Stay at the same state since the data is not received yet
+            //
+            state := sReadTypeOfOperator
+        }
+       }
+      is(sReadValueOfOperator) {
+
+        when(io.dataValidInput) {
+
+            //
+            // Configure the stages
+            //
+            configureStage := true.B
+
+            //
+            // Read the operator's "Value" data
+            //
+            regTargetOperator.Value := io.receivingData
+
+            //
+            // Check if reading is scripts are finished or not
+            //
+            when (regScriptNumberOfSymbols === 0.U) {
+                
+                //
+                // Configurartion was done
+                //
+                state := sDone
+            }.otherwise {
+
+            //
+            // Request next data
+            //
+            readNextData := true.B
+
+            //
+            // Again, read the next type
+            //
+            state := sReadTypeOfOperator
+
+            }
+        } .otherwise {
+
+            //
+            // Stay at the same state since the data is not received yet
+            //
+            state := sReadValueOfOperator
+        }
+      }
+      is(sDone) {
+
+        //
+        // Finished configuration
+        //
+        finishedConfiguration := true.B
+
+        //
+        // Move to the idle state
+        //
+        state := sIdle
+      }
+     }
+    }
 
   //
   // Connect output pins
   //
+  io.readNextData := readNextData
+
+  io.finishedConfiguration := finishedConfiguration
+  io.moveToNextStage := moveToNextStage
+  io.targetOperator := regTargetOperator
 
 }
 
@@ -70,7 +245,7 @@ object InterpreterScriptBufferHandler {
       en: Bool,
       dataValidInput: Bool,
       receivingData: UInt
-  ): (Bool, Bool, Bool, HwdbgShortSymbol) = {
+  ): (Bool, Bool, Bool, Bool, HwdbgShortSymbol) = {
 
     val interpreterScriptBufferHandler = Module(
       new InterpreterScriptBufferHandler(
@@ -83,6 +258,7 @@ object InterpreterScriptBufferHandler {
     val readNextData = Wire(Bool())
 
     val finishedConfiguration = Wire(Bool())
+    val configureStage = Wire(Bool())
     val moveToNextStage = Wire(Bool())
     val targetOperator = Wire(new HwdbgShortSymbol(instanceInfo.scriptVariableLength))
 
@@ -106,6 +282,7 @@ object InterpreterScriptBufferHandler {
     // Configure the output signals related to configuring stage operators
     //
     finishedConfiguration := interpreterScriptBufferHandler.io.finishedConfiguration
+    configureStage := interpreterScriptBufferHandler.io.configureStage
     moveToNextStage := interpreterScriptBufferHandler.io.moveToNextStage
     targetOperator := interpreterScriptBufferHandler.io.targetOperator
 
@@ -115,6 +292,7 @@ object InterpreterScriptBufferHandler {
     (
       readNextData,
       finishedConfiguration,
+      configureStage,
       moveToNextStage,
       targetOperator
     )
--- hwdbg/src/main/scala/hwdbg/main.scala
+++ hwdbg/src/main/scala/hwdbg/main.scala
@@ -49,6 +49,14 @@ class DebuggerMain(
     "err, the sum of the portsConfiguration (PORT_PINS_MAP) values must equal the numberOfPins (NUMBER_OF_PINS)."
   )
 
+  //
+  // Ensure script variable length is not bigger than BRAM data width
+  //
+  require(
+    bramDataWidth > scriptVariableLength,
+    "err, the script variable length should not be bigger than BRAM data width."
+  )
+
   val io = IO(new Bundle {
 
     //
