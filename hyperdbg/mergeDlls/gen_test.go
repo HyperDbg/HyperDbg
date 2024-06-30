5d93a90cdf41da0b3b866c78833a3e20f8b56b8e
Admin <2762713521@qq.com>
2024-07-01 01:25:50 +0800

    how to use

--- hyperdbg/mergeDlls/gen_test.go
+++ hyperdbg/mergeDlls/gen_test.go
@@ -2,6 +2,11 @@ package main
 
 /*
 1. copy D:\fork\HyperDbg\hyperdbg\FindWdk.cmake into C:\Windows
+2. build all projects ok
+3. rewrite all projects build config into one here
+4. get only one dll file with one .h file
+5. use clang command to check ast dumper
+6. start binding dll for golang
 
 What dll source and header files does libhyperdbg reference in addition to hyperlog?
  We need to sub here to merge the compilation configurations of all dependent projects so that the compilation results in only one unique dll,
