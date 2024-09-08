6949a85e3b9669f86da8199343634b227721ec98
SinaKarvandi <sina@hyperdbg.org>
2024-09-08 15:01:52 +0200

    finalize changelog for v0.10.1

--- hyperdbg/include/SDK/headers/Constants.h
+++ hyperdbg/include/SDK/headers/Constants.h
@@ -17,8 +17,8 @@
 //////////////////////////////////////////////////
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 11
-#define VERSION_PATCH 0
+#define VERSION_MINOR 10
+#define VERSION_PATCH 1
 
 //
 // Example of __DATE__ string: "Jul 27 2012"
