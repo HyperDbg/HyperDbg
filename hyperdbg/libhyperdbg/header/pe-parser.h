7b03230a421782ef29e9a38866836136d8607f02
alish14 <alish14.mod@gmail.com>
2025-08-04 05:21:16 +0330

    add Rich header entries struct

--- hyperdbg/libhyperdbg/header/pe-parser.h
+++ hyperdbg/libhyperdbg/header/pe-parser.h
@@ -23,3 +23,34 @@ PeIsPE32BitOr64Bit(const WCHAR * AddressOfFile, PBOOLEAN Is32Bit);
 
 UINT32
 PeGetSyscallNumber(LPCSTR NtFunctionName);
+int
+FindRichHeader(PIMAGE_DOS_HEADER dosHeader, char key[5]);
+
+void 
+SetRichEntries(int richHeaderSize, char* richHeaderPtr);
+
+void 
+FindRichEntries(char* richHeaderPtr, int richHeaderSize, char key[]);
+
+int 
+DecryptRichHeader(char key[], int index, char* dataPtr);
+
+
+typedef struct __RICH_HEADER_INFO {
+    int size;
+    char* ptrToBuffer;
+    int entries;
+} RICH_HEADER_INFO, * PRICH_HEADER_INFO;
+
+typedef struct __RICH_HEADER_ENTRY {
+    WORD prodID;
+    WORD buildID;
+    DWORD useCount;
+} RICH_HEADER_ENTRY, * PRICH_HEADER_ENTRY;
+
+typedef struct __RICH_HEADER {
+    PRICH_HEADER_ENTRY entries;
+} RICH_HEADER, * PRICH_HEADER;
+
+RICH_HEADER_INFO PEFILE_RICH_HEADER_INFO;
+RICH_HEADER PEFILE_RICH_HEADER;
